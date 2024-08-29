3c68b1105d086809d149afc03fe8493911d61835
Yusuf Eren Kaya <josepherenrock@hotmail.com>
2024-08-29 12:33:18 +0300

    Add files via upload

--- media_player_service/remote_media_player.py
+++ media_player_service/remote_media_player.py
@@ -1,10 +1,10 @@
-# remote_media_player.py
-
 import dbus.service
 import os
 from media import Media
 from audio import Audio
 from video import Video
+
+MY_INTERFACE = 'com.kentkart.RemoteMediaPlayer'
 
 class RemoteMediaPlayer(dbus.service.Object):
     def __init__(self, bus, object_path):
@@ -13,17 +13,18 @@ class RemoteMediaPlayer(dbus.service.Object):
         self._source_directories = []
         self._media_objects = {}
         self._object_id = 0
+        self.version = "1.0"
 
-    @dbus.service.method('com.kentkart.RemoteMediaPlayer', in_signature='s', out_signature='b')
+    @dbus.service.method(MY_INTERFACE, in_signature='s', out_signature='b')
     def AddSource(self, path):
         if os.path.isdir(path):
             self._source_directories.append(path)
             self.Scan()
-            self.PropertiesChanged(["AllMedia", "SourceDirectories"])  # Emitting a signal when properties change
+            self.PropertiesChanged(MY_INTERFACE, {"AllMedia": self.GetAllMedia(), "SourceDirectories": self._source_directories}, [])
             return True
         return False
 
-    @dbus.service.method('com.kentkart.RemoteMediaPlayer', in_signature='', out_signature='b')
+    @dbus.service.method(MY_INTERFACE, in_signature='', out_signature='b')
     def Scan(self):
         for directory in self._source_directories:
             if os.path.exists(directory):
@@ -37,40 +38,58 @@ class RemoteMediaPlayer(dbus.service.Object):
                     self._media_objects[object_path] = media
         return True
 
-    @dbus.service.method('com.kentkart.RemoteMediaPlayer', in_signature='', out_signature='b')
+    @dbus.service.method(MY_INTERFACE, in_signature='', out_signature='b')
     def ResetMedia(self):
         try:
-            # Unregistering all media objects from DBus
             for object_path, media_object in self._media_objects.items():
                 media_object.remove_from_connection()
-            # Clearing the dictionary for media objects
             self._media_objects.clear()
-            
-            # Emitting a signal to indicate that AllMedia is changed
-            self.PropertiesChanged(["AllMedia"])
-
+            self.PropertiesChanged(MY_INTERFACE, {"AllMedia": []}, [])
             print("All media have been reset and unregistered from DBus.")
             return True
         except Exception as e:
             print(f"Failed to reset media: {e}")
             return False
 
-    @dbus.service.signal('com.kentkart.RemoteMediaPlayer', signature='as')
-    def PropertiesChanged(self, properties):
-        print(f"Properties changed: {properties}")
+    # Signal method to indicate property changes
+    @dbus.service.signal(dbus.PROPERTIES_IFACE, signature='sa{sv}as')
+    def PropertiesChanged(self, interface_name, changed_properties, invalidated_properties):
+        print(f"Properties changed: {changed_properties}")
 
+    # Method to get properties based on the D-Bus interface
     @dbus.service.method(dbus.PROPERTIES_IFACE, in_signature='ss', out_signature='v')
     def Get(self, interface_name, property_name):
-        if property_name == 'AllMedia':
-            return self.GetAllMedia()
+        if interface_name == MY_INTERFACE:
+            if property_name == 'AllMedia':
+                return list(self._media_objects.keys())
+            elif property_name == 'SourceDirectories':
+                return self._source_directories
+            elif property_name == 'Version':
+                return self.version
         raise dbus.exceptions.DBusException('org.freedesktop.DBus.Error.UnknownProperty',
                                             'No such property {}'.format(property_name))
 
-    @dbus.service.method('com.kentkart.RemoteMediaPlayer', in_signature='', out_signature='as')
+    @dbus.service.method(dbus.PROPERTIES_IFACE, in_signature='s', out_signature='a{sv}')
+    def GetAll(self, interface_name):
+        if interface_name == MY_INTERFACE:
+            return {
+                'Version': self.version,
+                'SourceDirectories': self._source_directories,
+                'AllMedia': list(self._media_objects.keys())
+            }
+        else:
+            raise dbus.exceptions.DBusException(
+                'com.kentkart.UnknownInterface',
+                f'The object does not implement the {interface_name} interface'
+            )
+
+
+
+    # Method to get all media objects
+    @dbus.service.method(MY_INTERFACE, in_signature='', out_signature='as')
     def GetAllMedia(self):
         if not self._media_objects:
-            raise dbus.exceptions.DBusException('org.freedesktop.DBus.Error.NoMedia',
-                                                'No media available')
+            raise dbus.exceptions.DBusException('org.freedesktop.DBus.Error.NoMedia', 'No media available')
         return list(self._media_objects.keys())
 
     def generate_object_path(self):
