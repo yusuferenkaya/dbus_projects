2a7690173b6e35d1e8f3523d87bfc946f69cad94
Yusuf Eren Kaya <josepherenrock@hotmail.com>
2024-09-09 17:01:02 +0300

    Delete media_player_service/interface_names.py

--- media_player_service/interface_names.py
+++
@@ -1,5 +0,0 @@
-REMOTE_MEDIA_PLAYER_INTERFACE = "com.kentkart.RemoteMediaPlayer"
-MEDIA_INTERFACE = "com.kentkart.RemoteMediaPlayer.Media"
-AUDIO_INTERFACE = "com.kentkart.RemoteMediaPlayer.Media.Audio"
-VIDEO_INTERFACE = "com.kentkart.RemoteMediaPlayer.Media.Video"
-PROPERTIES_INTERFACE = "org.freedesktop.DBus.Properties"
